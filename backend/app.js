06814a6dd62edc3ecbae21fab74decf64113ee04
One-Solutions-2024 <onesolutions06092024@gmail.com>
2024-12-01 19:54:23 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -51,7 +51,7 @@ fs.mkdir(uploadsDir, { recursive: true })
 // Multer setup for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "uploads/"),
-  filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
+  filename: (req, file, cb) => cb(null, `${file.originalname}`),
 });
 const upload = multer({
   storage,
