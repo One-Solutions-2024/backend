1f27688e5ba1a8a86d25912c3cee8eaac2169e16
One-Solutions-2024 <onesolutions06092024@gmail.com>
2024-12-02 18:42:58 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,6 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
 app.use(helmet());
 app.use(morgan("combined"));
 app.use(bodyParser.json());
@@ -51,29 +50,8 @@ fs.mkdir("uploads",  { recursive: true })
   .catch((err) => console.error("Failed to create uploads directory:", err));
 
 
-  // Set up CORS to allow all origins for image serving
-// Set up CORS for image serving
-// Set up CORS for image serving with Cache-Control headers
-app.use("/uploads", (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow any origin
-  res.setHeader("Access-Control-Allow-Methods", "GET"); // Allow only GET for static files
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Allow content type headers
-  
-  // Set Cache-Control headers to prevent caching
-  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // No caching
-  res.setHeader("Pragma", "no-cache"); // HTTP 1.0 compatibility
-  res.setHeader("Expires", "0"); // No expiration
-
-  next();
-});
-
-const corsOptions = {
-  origin: '*', // Allow all origins
-  methods: ['GET'], // Allow only GET method
-};
-
-app.use('/uploads', cors(corsOptions), express.static('uploads'));
-
+ // Serve static files from the uploads directory
+app.use("/uploads", express.static("uploads"));
 
 // Multer setup for file uploads
 const storage = multer.diskStorage({
